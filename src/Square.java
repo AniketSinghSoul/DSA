a4aad1d1cb995a706db1cc5771d48dc1c6cbcfd7
AniketSingh <tmred444ffiit@gmail.com>
2026-08-05 09:22:49 +0530

    First practice

--- src/Square.java
+++ src/Square.java
@@ -1,2 +1,14 @@
 public class Square {
+    public static int main(String[] args) {
+        int factorial;
+      int n = 0;  
+        
+            int fact = 1;
+            for (int i = 1; i <= n; i++)
+                fact = fact * i;
+            return 0;
+        
+    }
 }
+
+
